e6b53528f286937dd2837a1e7dafa9d90543d10e
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2012-02-29 21:16:16 -0800

    Implemented BlockingQueue with optional per-item size for throttling (i.e. to restrict total amount of memory used by queued items)

---
+++ src/blockingqueue.hpp
@@ -0,0 +1,77 @@
+#ifndef BLOCKINGQUEUE_HPP
+#define BLOCKINGQUEUE_HPP
+
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <cassert>
+
+template <typename T>
+class BlockingQueue
+{
+public:
+	BlockingQueue(): totalSize(0), totalSizeLimit(static_cast<size_t>(-1))
+	{
+	}
+
+	explicit BlockingQueue(size_t limit): totalSize(0), totalSizeLimit(limit)
+	{
+	}
+
+	void push(const T& value, size_t size = 0)
+	{
+		std::unique_lock<std::mutex> lock(mutex);
+
+		itemsNotFull.wait(lock, [&]() { return !(totalSize != 0 && totalSize + size > totalSizeLimit); });
+
+		bool wasEmpty = items.empty();
+
+		Item item = {value, size};
+		items.push(item);
+		totalSize += size;
+
+		if (wasEmpty)
+		{
+			lock.unlock();
+			itemsNotEmpty.notify_one();
+		}
+	}
+
+	T pop()
+	{
+		std::unique_lock<std::mutex> lock(mutex);
+
+		itemsNotEmpty.wait(lock, [&]() { return !items.empty(); });
+
+		Item item = items.front();
+		items.pop();
+
+		if (item.size > 0)
+		{
+			assert(totalSize >= item.size);
+			totalSize -= item.size;
+
+			lock.unlock();
+			itemsNotFull.notify_all();
+		}
+
+		return item.value;
+	}
+
+private:
+	struct Item
+	{
+		T value;
+		size_t size;
+	};
+
+	std::mutex mutex;
+	std::condition_variable itemsNotEmpty;
+	std::condition_variable itemsNotFull;
+
+	std::queue<Item> items;
+	size_t totalSize;
+	size_t totalSizeLimit;
+};
+
+#endif
