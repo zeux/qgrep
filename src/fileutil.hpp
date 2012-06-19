d5c9dd25f120e1c9608e1fb85d1755994047ba2a
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2012-06-19 22:24:35 +0400

    Move Win32-specific path handling code to a separate file

--- src/fileutil.hpp
+++ src/fileutil.hpp
@@ -7,6 +7,7 @@
 bool traverseDirectory(const char* path, const std::function<void (const char* name)>& callback);
 bool traverseDirectoryMeta(const char* path, const std::function<void (const char* name, uint64_t mtime, uint64_t size)>& callback);
 
+void createDirectory(const char* path);
 void createPath(const char* path);
 void createPathForFile(const char* path);
 bool renameFile(const char* oldpath, const char* newpath);
---
+++ src/fileutil_win.cpp
@@ -0,0 +1,124 @@
+#ifdef _WIN32
+
+#include "fileutil.hpp"
+
+#include <string>
+#include <vector>
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+static uint64_t combine(uint32_t hi, uint32_t lo)
+{
+	return (static_cast<uint64_t>(hi) << 32) | lo;
+}
+
+static bool processFile(const char* name)
+{
+	if (name[0] == '.')
+	{
+		// pseudo-folders
+		if (strcmp(name, ".") == 0 || strcmp(name, "..") == 0) return false;
+	
+		// VCS folders
+		if (strcmp(name, ".svn") == 0 || strcmp(name, ".hg") == 0 || strcmp(name, ".git") == 0) return false;
+	}
+
+	return true;
+}
+
+static void concatPathName(std::string& buf, const char* path, const char* name)
+{
+	buf = path;
+	if (*path) buf += "/";
+	buf += name;
+}
+
+static bool readDirectory(const char* path, std::vector<WIN32_FIND_DATAA>& result)
+{
+	std::string query = std::string(path) + "/*";
+
+	WIN32_FIND_DATAA data;
+	HANDLE h = FindFirstFileA(query.c_str(), &data);
+
+	if (h != INVALID_HANDLE_VALUE)
+	{
+		do result.push_back(data);
+		while (FindNextFileA(h, &data));
+
+		FindClose(h);
+	}
+
+	return h != INVALID_HANDLE_VALUE;
+}
+
+static bool traverseDirectoryImpl(const char* path, const char* relpath, const std::function<void (const char* name, uint64_t mtime, uint64_t size)>& callback)
+{
+	std::vector<WIN32_FIND_DATAA> contents;
+	contents.reserve(16);
+
+	if (readDirectory(path, contents))
+	{
+		std::string buf, relbuf;
+
+		for (auto& data: contents)
+		{
+			if (processFile(data.cFileName))
+			{
+				concatPathName(relbuf, relpath, data.cFileName);
+
+				if (data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+				{
+					concatPathName(buf, path, data.cFileName);
+					traverseDirectoryImpl(buf.c_str(), relbuf.c_str(), callback);
+				}
+				else
+				{
+					uint64_t mtime = combine(data.ftLastWriteTime.dwHighDateTime, data.ftLastWriteTime.dwLowDateTime);
+					uint64_t size = combine(data.nFileSizeHigh, data.nFileSizeLow);
+					callback(relbuf.c_str(), mtime, size);
+				}
+			}
+		}
+
+		return true;
+	}
+
+	return false;
+}
+
+bool traverseDirectory(const char* path, const std::function<void (const char* name)>& callback)
+{
+	return traverseDirectoryImpl(path, "", [&](const char* name, uint64_t, uint64_t) { callback(name); });
+}
+
+bool traverseDirectoryMeta(const char* path, const std::function<void (const char* name, uint64_t mtime, uint64_t size)>& callback)
+{
+	return traverseDirectoryImpl(path, "", callback);
+}
+
+bool renameFile(const char* oldpath, const char* newpath)
+{
+	return !!MoveFileExA(oldpath, newpath, MOVEFILE_REPLACE_EXISTING);
+}
+
+bool getFileAttributes(const char* path, uint64_t* mtime, uint64_t* size)
+{
+	WIN32_FILE_ATTRIBUTE_DATA data;
+
+	if (GetFileAttributesExA(path, GetFileExInfoStandard, &data))
+	{
+		*mtime = combine(data.ftLastWriteTime.dwHighDateTime, data.ftLastWriteTime.dwLowDateTime);
+		*size = combine(data.nFileSizeHigh, data.nFileSizeLow);
+		return true;
+	}
+
+	return false;
+}
+
+void createDirectory(const char* path)
+{
+    CreateDirectoryA(path, NULL);
+}
+
+#endif
