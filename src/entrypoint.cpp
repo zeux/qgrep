b5b0f6bc4181835bba68c8b695e170d4b82c2671
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2012-07-05 13:34:46 +0400

    Revert accidental debug changes

--- src/entrypoint.cpp
+++ src/entrypoint.cpp
@@ -4,12 +4,7 @@ __declspec(dllimport)
 #endif
 void entryPointConsole(int argc, const char** argv);
 
-#include <stdlib.h>
-
 int main(int argc, const char** argv)
 {
-	_putenv_s("HOME", "C:/Users/Zeux");
-
-	// for (int i = 0; i < 1000; ++i)
 	entryPointConsole(argc, argv);
 }
