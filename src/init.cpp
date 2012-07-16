67ed389b7c2a34bfe07da60200267d32d1856ee7
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2012-07-16 15:39:15 +0400

    Init now normalizes the source path relative to current directory.
    This allows to call qgrep init with relative path and later call qgrep build from
    another working directory.

--- src/init.cpp
+++ src/init.cpp
@@ -29,9 +29,12 @@ void initProject(Output* output, const char* name, const char* file, const char*
 		output->error("Error opening project file %s for writing\n", file);
 		return;
 	}
+
+	std::string cwd = getCurrentDirectory();
+	std::string npath = normalizePath(cwd.c_str(), path);
 	
-	out << "path " << path << std::endl;
+	out << "path " << npath << std::endl;
 	out << "include \\.(cpp|cxx|cc|c|hpp|hxx|hh|h|inl|py|pl|pm|js|as|hlsl|cg|fx)$" << std::endl;
 
-	output->print("Project file %s created, run 'qgrep build %s' to build\n", file, name);
+	output->print("Project file %s created for folder %s, run 'qgrep build %s' to build\n", file, npath.c_str(), name);
 }
