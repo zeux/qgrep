9cdf97c85c67fd9579eb2da43fbbee10274b153b
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2016-06-10 21:54:02 -0700

    Add include for memchr

--- src/filterutil.cpp
+++ src/filterutil.cpp
@@ -7,6 +7,7 @@
 
 #include <vector>
 
+#include <string.h>
 #include <stdio.h>
 
 unsigned int filterBuffer(Output* output, const char* string, unsigned int options, unsigned int limit, const char* buffer, size_t bufferSize)
