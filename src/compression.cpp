10c75ea5b5351751c0219f0babed16839e43f344
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2017-01-01 13:59:42 -0800

    Enhance compression API
    Add compression level as an explicit argument to compress so that the
    caller can decide, add partial decompression so that we can decompress a
    prefix of the data if we only need that prefix.

--- src/compression.cpp
+++ src/compression.cpp
@@ -4,16 +4,15 @@
 #include "lz4.h"
 #include "lz4hc.h"
 
-std::pair<std::unique_ptr<char[]>, size_t> compress(const void* data, size_t dataSize)
+std::pair<std::unique_ptr<char[]>, size_t> compress(const void* data, size_t dataSize, int level)
 {
 	if (dataSize == 0) return std::make_pair(std::unique_ptr<char[]>(), 0);
 
-	int compressionLevel = LZ4HC_DEFAULT_CLEVEL;
 	int csizeBound = LZ4_compressBound(dataSize);
 
 	std::unique_ptr<char[]> cdata(new char[csizeBound]);
 	
-	int csize = LZ4_compress_HC(static_cast<const char*>(data), cdata.get(), dataSize, csizeBound, compressionLevel);
+	int csize = LZ4_compress_HC(static_cast<const char*>(data), cdata.get(), dataSize, csizeBound, level);
 	assert(csize >= 0 && csize <= csizeBound);
 
 	return std::make_pair(std::move(cdata), csize);
@@ -26,4 +25,15 @@ void decompress(void* dest, size_t destSize, const void* source, size_t sourceSi
 	int result = LZ4_decompress_fast(static_cast<const char*>(source), static_cast<char*>(dest), destSize);
 	assert(result >= 0);
 	assert(static_cast<size_t>(result) == sourceSize);
+}
+
+void decompressPartial(void* dest, size_t destSize, const void* source, size_t sourceSize, size_t targetSize)
+{
+	assert(targetSize <= destSize);
+	if (sourceSize == 0 && destSize == 0) return;
+
+	int result = LZ4_decompress_safe_partial(static_cast<const char*>(source), static_cast<char*>(dest), sourceSize, targetSize, destSize);
+	assert(result >= 0);
+	assert(static_cast<size_t>(result) >= targetSize);
+	assert(static_cast<size_t>(result) <= destSize);
 }
--- src/compression.hpp
+++ src/compression.hpp
@@ -3,6 +3,7 @@
 #include <memory>
 #include <utility>
 
-std::pair<std::unique_ptr<char[]>, size_t> compress(const void* data, size_t dataSize);
+std::pair<std::unique_ptr<char[]>, size_t> compress(const void* data, size_t dataSize, int level);
 
 void decompress(void* dest, size_t destSize, const void* source, size_t sourceSize);
+void decompressPartial(void* dest, size_t destSize, const void* source, size_t sourceSize, size_t targetSize);
