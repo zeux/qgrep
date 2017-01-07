eb99e55c33c2ac7b690051a7835637cdffc71a16
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2017-01-07 10:49:20 -0800

    watch: Implement change monitoring
    Whenever a file changes we now update the .qgc file with a new list of
    changed files as long as the file matches the project group filters.
    
    There is currently no throttling on the .qgc update so this may be
    suboptimal for large batches of operations such as git checkout.
    Additionally, the starting point is currently an empty set of files so
    repeatedly running watch will not work well. This will be addressed
    later.

--- src/watch.cpp
+++ src/watch.cpp
@@ -3,26 +3,86 @@
 
 #include "project.hpp"
 #include "fileutil.hpp"
+#include "filestream.hpp"
 #include "output.hpp"
 
+#include <set>
 #include <thread>
+#include <mutex>
+#include <condition_variable>
 
 struct WatchContext
 {
 	Output* output;
 
+	std::thread updateThread;
 	std::vector<std::thread> watchingThreads;
+
+	std::set<std::string> changedFiles;
+	std::mutex changedFilesMutex;
+	std::condition_variable changedFilesChanged;
 };
 
 static void fileChanged(WatchContext* context, ProjectGroup* group, const char* path, const char* file)
 {
 	if (isFileAcceptable(group, file))
 	{
-		context->output->print("File changed: %s\n", file);
+		std::string npath = normalizePath(path, file);
+
+		context->output->print("Change %s\n", npath.c_str());
+
+		std::unique_lock<std::mutex> lock(context->changedFilesMutex);
+
+		context->changedFiles.insert(npath);
+		context->changedFilesChanged.notify_one();
 	}
 	else
 	{
-		context->output->print("File changed: %s (ignore)\n", file);
+		std::string npath = normalizePath(path, file);
+
+		context->output->print("Ignore %s\n", npath.c_str());
+	}
+}
+
+static void updateThreadFunc(WatchContext* context, const char* path)
+{
+	std::string targetPath = replaceExtension(path, ".qgc");
+	std::string tempPath = targetPath + "_";
+
+	std::vector<std::string> changedFiles;
+
+	for (;;)
+	{
+		{
+			std::unique_lock<std::mutex> lock(context->changedFilesMutex);
+
+			context->changedFilesChanged.wait(lock, [&] { return context->changedFiles.size() != changedFiles.size(); });
+
+			changedFiles.assign(context->changedFiles.begin(), context->changedFiles.end());
+		}
+
+		context->output->print("Flush %d\n", int(changedFiles.size()));
+
+		{
+			FileStream out(tempPath.c_str(), "wb");
+			if (!out)
+			{
+				context->output->error("Error saving changes to %s\n", tempPath.c_str());
+				continue;
+			}
+
+			for (auto& f : changedFiles)
+			{
+				out.write(f.data(), f.size());
+				out.write("\n", 1);
+			}
+		}
+
+		if (!renameFile(tempPath.c_str(), targetPath.c_str()))
+		{
+			context->output->error("Error saving changes to %s\n", targetPath.c_str());
+			continue;
+		}
 	}
 }
 
@@ -36,6 +96,8 @@ static void startWatchingRec(WatchContext* context, ProjectGroup* group)
 
 			if (!watchDirectory(path.c_str(), [=](const char* file) { fileChanged(context, group, path.c_str(), file); }))
 				context->output->error("Error watching folder %s\n", path.c_str());
+
+			context->output->print("No longer watching folder %s\n", path.c_str());
 		});
 	}
 
@@ -51,6 +113,10 @@ void watchProject(Output* output, const char* path)
 
 	startWatchingRec(&context, group.get());
 
+	std::thread([&] { updateThreadFunc(&context, path); }).swap(context.updateThread);
+
 	for (auto& t: context.watchingThreads)
 		t.join();
+
+	context.updateThread.join();
 }
