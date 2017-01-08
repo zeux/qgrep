8ab3d3e9508f52475dc66c6baad364da134572df
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2017-01-08 09:16:20 -0800

    Cleanup common.h
    Remove stdlib.h and remove other includes of these headers from all
    source files.

--- src/common.hpp
+++ src/common.hpp
@@ -2,5 +2,4 @@
 
 #include <stddef.h>
 #include <stdint.h>
-#include <stdlib.h>
 #include <assert.h>
--- src/fileutil_win.cpp
+++ src/fileutil_win.cpp
@@ -7,8 +7,6 @@
 #include <vector>
 #include <algorithm>
 
-#include <assert.h>
-
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
