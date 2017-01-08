5e26b42db7a60a155d22f9ae06cb68839377069a
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2017-01-08 09:04:32 -0800

    watch: Implement initial change list retrieval
    When watch starts we now read the data pack, extract the file table and
    compare it with the file table we got via filesystem scan to determine
    the initial set of changes to write to .qgc file.
    
    This is basically the same process that update performs, but implemented
    somewhat differently.
    
    This code is in dire need of refactoring and it's interesting to
    consider reusing update code for this, but it seems to work.
    
    File removals are not currently handled because it's not clear if it's a
    good idea to write them to changed list.

--- src/watch.cpp
+++ src/watch.cpp
@@ -5,11 +5,14 @@
 #include "fileutil.hpp"
 #include "filestream.hpp"
 #include "output.hpp"
+#include "format.hpp"
+#include "compression.hpp"
 
 #include <set>
 #include <thread>
 #include <mutex>
 #include <condition_variable>
+#include <algorithm>
 
 struct WatchContext
 {
@@ -90,10 +93,10 @@ static void startWatchingRec(WatchContext* context, ProjectGroup* group)
 {
 	for (auto& path : group->paths)
 	{
+		context->output->print("Watching folder %s...\n", path.c_str());
+
 		context->watchingThreads.emplace_back([=]
 		{
-			context->output->print("Watching folder %s...\n", path.c_str());
-
 			if (!watchDirectory(path.c_str(), [=](const char* file) { fileChanged(context, group, path.c_str(), file); }))
 				context->output->error("Error watching folder %s\n", path.c_str());
 
@@ -105,13 +108,151 @@ static void startWatchingRec(WatchContext* context, ProjectGroup* group)
 		startWatchingRec(context, child.get());
 }
 
+static bool getDataFileList(Output* output, const char* path, std::vector<FileInfo>& result)
+{
+	FileStream in(path, "rb");
+	if (!in)
+	{
+		output->error("Error reading data file %s\n", path);
+		return false;
+	}
+
+	DataFileHeader header;
+	if (!read(in, header) || memcmp(header.magic, kDataFileHeaderMagic, strlen(kDataFileHeaderMagic)) != 0)
+	{
+		output->error("Error reading data file %s: file format is out of date, update the project to fix\n", path);
+		return false;
+	}
+
+	DataChunkHeader chunk;
+	std::vector<char> data;
+
+	while (read(in, chunk))
+	{
+		try
+		{
+			data.resize(chunk.compressedSize + chunk.uncompressedSize);
+		}
+		catch (const std::bad_alloc&)
+		{
+			output->error("Error reading data file %s: malformed chunk\n", path);
+			return false;
+		}
+
+		in.skip(chunk.extraSize);
+		in.skip(chunk.indexSize);
+
+		if (!read(in, data.data(), chunk.compressedSize))
+		{
+			output->error("Error reading data file %s: malformed chunk\n", path);
+			return false;
+		}
+
+		char* uncompressed = data.data() + chunk.compressedSize;
+
+		decompressPartial(uncompressed, chunk.uncompressedSize, data.data(), chunk.compressedSize, chunk.fileTableSize);
+
+		const DataChunkFileHeader* files = reinterpret_cast<const DataChunkFileHeader*>(uncompressed);
+
+		for (unsigned int i = 0; i < chunk.fileCount; ++i)
+		{
+			const DataChunkFileHeader& file = files[i];
+
+			FileInfo fi(std::string(uncompressed + file.nameOffset, file.nameLength), file.timeStamp, file.fileSize);
+
+			if (result.empty() || result.back().path != fi.path)
+				result.push_back(fi);
+		}
+	}
+
+	return true;
+}
+
 void watchProject(Output* output, const char* path)
 {
 	WatchContext context = { output };
 
+    output->print("Watching %s:\n", path);
+
 	std::unique_ptr<ProjectGroup> group = parseProject(output, path);
+	if (!group)
+		return;
 
 	startWatchingRec(&context, group.get());
+
+	output->print("Scanning project...\r");
+
+	std::vector<FileInfo> files = getProjectGroupFiles(output, group.get());
+
+	output->print("Reading data pack...\r");
+
+	std::vector<FileInfo> packFiles;
+	if (!getDataFileList(output, replaceExtension(path, ".qgd").c_str(), packFiles))
+		return;
+
+	std::vector<std::string> changedFiles;
+	unsigned int filesAdded = 0;
+	unsigned int filesRemoved = 0;
+	unsigned int filesChanged = 0;
+
+	for (size_t i = 0, j = 0; i < files.size() || j < packFiles.size(); )
+	{
+		if (i < files.size() && j < packFiles.size())
+		{
+			if (files[i].path == packFiles[j].path)
+			{
+				if (files[i].fileSize != packFiles[j].fileSize || files[i].lastWriteTime != packFiles[j].lastWriteTime)
+				{
+					changedFiles.push_back(files[i].path);
+					filesChanged++;
+				}
+
+				i++;
+				j++;
+			}
+			else
+			{
+				if (files[i].path < packFiles[j].path)
+				{
+					changedFiles.push_back(files[i].path);
+					filesAdded++;
+					i++;
+				}
+				else
+				{
+					// TODO: handle removed files
+					filesRemoved++;
+					j++;
+				}
+			}
+		}
+		else if (i < files.size())
+		{
+			changedFiles.push_back(files[i].path);
+			filesAdded++;
+			i++;
+		}
+		else if (j < packFiles.size())
+		{
+			// TODO: handle removed files
+			filesRemoved++;
+			j++;
+		}
+	}
+
+	removeFile(replaceExtension(path, ".qgc").c_str());
+
+	{
+		std::unique_lock<std::mutex> lock(context.changedFilesMutex);
+
+		context.changedFiles.insert(changedFiles.begin(), changedFiles.end());
+	}
+
+	if (filesAdded) output->print("+%d ", filesAdded);
+	if (filesRemoved) output->print("-%d ", filesRemoved);
+	if (filesChanged) output->print("*%d ", filesChanged);
+	output->print("%s; listening for further changes\n",
+		(filesAdded || filesRemoved || filesChanged) ? "files" : "No changes");
 
 	std::thread([&] { updateThreadFunc(&context, path); }).swap(context.updateThread);
 
